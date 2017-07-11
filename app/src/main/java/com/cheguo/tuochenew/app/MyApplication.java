ea403b4dcbe629d52f5a8c061c6fdf78b2abdb5a
陈瑶 <michael_cyrus@163.com>
2017-07-11 18:21:19 +0800

    添加依赖 Logger Bugly

--- app/src/main/java/com/cheguo/tuochenew/app/MyApplication.java
+++ app/src/main/java/com/cheguo/tuochenew/app/MyApplication.java
@@ -1,10 +1,17 @@
 package com.cheguo.tuochenew.app;
 
 import android.app.Application;
+import android.content.Context;
+import android.text.TextUtils;
 
 import com.cheguo.tuochenew.BuildConfig;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
+import com.tencent.bugly.crashreport.CrashReport;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by chenyao on 2017/7/11.
@@ -16,6 +23,14 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         MyApp.getInstance().init(this);
+
+        initBugly();
+
+        initLog();
+
+    }
+
+    private void initLog() {
         Logger.addLogAdapter(new AndroidLogAdapter(){
             @Override
             public boolean isLoggable(int priority, String tag) {
@@ -23,5 +38,54 @@ public class MyApplication extends Application {
             }
         });
         Logger.e("MyApplication 初始化完成。。。");
+    }
+
+    /**
+     * Tencent bugly 配置
+     * 第三个参数为SDK调试模式开关，调试模式的行为特性如下：
+         输出详细的Bugly SDK的Log；
+         每一条Crash都会被立即上报；
+         自定义日志将会在Logcat中输出。
+         建议在测试阶段建议设置成true，发布时设置为false。
+     */
+    private void initBugly() {
+        Context context = getApplicationContext();
+        // 获取当前包名
+        String packageName = context.getPackageName();
+        // 获取当前进程名
+        String processName = getProcessName(android.os.Process.myPid());
+        // 设置是否为上报进程
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setUploadProcess(processName == null || processName.equals(packageName));
+        // 初始化Bugly
+        CrashReport.initCrashReport(context, "9a26be29d3", BuildConfig.DEBUG, strategy);
+    }
+
+    /**
+     * 获取进程号对应的进程名
+     * @param pid 进程号
+     * @return 进程名
+     */
+    private static String getProcessName(int pid) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+            String processName = reader.readLine();
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim();
+            }
+            return processName;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return null;
     }
 }
